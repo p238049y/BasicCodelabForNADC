dc85ba15c2f872776a2df5c05b485f5769886aac
p238049y <p238049y@gmail.com>
2023-10-14 01:35:07 +0900

    [update] アプリのスタイルとテーマを設定する

--- app/src/main/java/com/example/basiccodelabfornadc/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelabfornadc/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccodelabfornadc.ui.theme.BasicCodelabForNADCTheme
@@ -78,7 +79,11 @@ private fun Greeting(name: String) {
                 .weight(1f)
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded },
